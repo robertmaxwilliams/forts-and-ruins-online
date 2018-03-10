7cef5da42f66b0f47f3177b523fa4b7c1b280d72
Me 261998 <261998@fortsandruins.nfshost.com>
2018-03-10 01:53:35 +0000

    done with server testing, made server.go to begin work

--- server.go
+++ server.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "buts there, I love %s!", "yyesyeses" +r.URL.Path[1:])
+    fmt.Fprintf(w, "tuuuuuuussss there, I love %s!", "yyesyeses" +r.URL.Path[1:])
 }
 
 func main() {
